0410b09544b04b4bc5a30a8d8b05f54dc770a125
V2-hub <teknobot24@gmail.com>
2026-06-25 20:14:39 +0530

    Adding Fake Data for FastApi

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+
+def read_root():
+    return{"Message":"Welcome to DevVault"}
+
+commands = [
+    {
+        "command": "git status",
+        "description": "Check the status of your git repository",
+        "category": "Git"
+    }
+]
